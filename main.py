432b7e1db29e6c96dd664265e00579b6761ee412
Fiodor <107990040+fedyaded@users.noreply.github.com>
2022-07-27 00:25:20 +0300

    update keyboard

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.dispatcher.filters import Text
+import markups as nav
 
 
 bot = Bot(token=tg_bot_token,parse_mode='HTML')
@@ -12,12 +13,21 @@ dp = Dispatcher(bot)
 # Команды + Клавиатура
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["Результаты 🏎", "Следующая гонка ➡", "Чемпионат 🏆"]
-    keyboard.add(*buttons)
-    await message.answer("Привет, "+message.from_user.first_name + "\nЯ F1-bot", reply_markup=keyboard)
+    await message.answer("Привет, "+message.from_user.first_name + "\nЯ F1-bot", reply_markup= nav.mainMenu)
+
+
+@dp.message_handler(Text(equals="Главное меню"))
+async def with_puree(message: types.Message):
+    if message.text == "Главное меню":
+        await message.answer("Выбирай", reply_markup=nav.mainMenu)
+
 
 @dp.message_handler(Text(equals="Результаты 🏎"))
+async def with_puree(message:types.Message):
+    if message.text == "Результаты 🏎":
+        await message.answer("Что хочешь узнать?", reply_markup=nav.otherMenu)
+
+@dp.message_handler(Text(equals="Результаты гонки"))
 async def with_puree(message: types.Message):
     r = requests.get(
         f"http://ergast.com/api/f1/current/last/results.json"
@@ -55,8 +65,30 @@ async def with_puree(message: types.Message):
                         driver['points'] + "\n"
             except Exception:
                 pass
-    await message.answer(text)
+    if message.text == "Чемпионат 🏆":
+        await message.answer(text)
 
+@dp.message_handler(Text(equals="Кубок конструкторов"))
+async def with_puree(message: types.Message):
+    r = requests.get(
+        f"http://ergast.com/api/f1/current/constructorStandings.json"
+    )
+    data = r.json()["MRData"]["StandingsTable"]["StandingsLists"][0]["ConstructorStandings"]
+
+    text = "<b>Команда - Очки</b>\n\n"
+
+    for team in data:
+        try:
+            text += team["Constructor"]["name"] + " - " + \
+                    team['Points']['points'] + "\n"
+        except KeyError:
+            try:
+                text += team["Constructor"]["name"] + " - " + \
+                        team['points'] + "\n"
+            except Exception:
+                pass
+    if message.text == "Кубок конструкторов":
+        await message.answer(text)
 
 
 @dp.message_handler(lambda message: message.text == "Следующая гонка ➡")
@@ -68,7 +100,8 @@ async def without_puree(message: types.Message):
 
     text ="<b>\nТрасса: </b>"+data['raceName']+ "<b>\nДата: </b>" + data["date"] + "<b>\nВремя: </b>"+ data["time"].replace("Z","")
 
-    await message.answer(text)
+    if message.text == "Следующая гонка ➡":
+        await message.answer(text)
 
 
 #Запуск бота
