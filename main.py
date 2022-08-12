7ff3174fb0ed5c551c2872889fcdf4df796b122c
Fiodor <107990040+fedyaded@users.noreply.github.com>
2022-08-12 20:09:41 +0300

    new keyboard

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 from db import Database
 import markups as nav
-from config import tg_bot_token,ADMIN_TOKEN
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=tg_bot_token,parse_mode='HTML')
+bot = Bot(token="5596538362:AAHLseYDoev0DOia8h5a9tTj_-vKTlLSE3M")
 dp = Dispatcher(bot)
 db = Database('database.db')
+ADMIN_TOKEN = 434892676
 
 
 @dp.message_handler(commands=['start'])
